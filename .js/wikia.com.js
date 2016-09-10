b910dc6bc06b22d3bcbf11d4820a7399e4ad80e1
Shadowfacts <me@shadowfacts.net>
2016-09-10 11:04:27 -0400

    Add wikia.com.js

---
+++ .js/wikia.com.js
@@ -0,0 +1,14 @@
+// remove "Trending Fandom Articles" section
+(function() {
+	$("section.group-FANDOM_TOPIC").hide();
+})();
+
+// remove Wikia notifications
+(function() {
+	$("ul#WikiaNotifications").hide();
+})();
+
+// remove Wikia bar
+(function() {
+	$("div#WikiaBar").hide();
+})();

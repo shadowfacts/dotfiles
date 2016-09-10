8dc3589f02a539b7ba59e49a55dcb68d77caa2e1
Shadowfacts <me@shadowfacts.net>
2016-09-10 12:50:13 -0400

    Fix RES expando replacing

--- .js/com/reddit.js
+++ .js/com/reddit.js
@@ -1,16 +1,19 @@
 // replace RES expando icons with reddit ones
 (function() {
-	$("a.expando-button").each((i, el) => {
-		el = $(el);
-		if (!el.hasClass("selftext") && !el.hasClass("video")) {
-			el.addClass("video");
-			el.click(() => {
-				if (el.hasClass("expanded")) {
+	function fixExpandos() {
+		$("a.expando-button").each((i, el) => {
+			el = $(el);
+			if (!el.hasClass("selftext") && !el.hasClass("video")) {
+				el.removeClass("image");
+				el.removeClass("gallery");
+				el.addClass("video");
+				el.click(() => {
+					el.removeClass("image");
+					el.removeClass("gallery");
 					el.addClass("video");
-				} else {
-					el.addClass("video");
-				}
-			});
-		}
-	});
+				});
+			}
+		});
+	}
+	setTimeout(fixExpandos, 50);
 })();

168b9600eea54c1b540ac35bc3457372cd70c66b
Shadowfacts <shadowfacts@users.noreply.github.com>
2016-09-17 09:55:49 -0400

    Fix hammerspoon config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -35,9 +35,9 @@ hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 local caffeine = hs.menubar.new()
 function setCaffeineDisplay(state)
 	if state then
-		caffeine:setIcon("caffeine-on.pdf")
+		caffeine:setIcon("~/.hammerspoon/caffeine-on.pdf")
 	else
-		caffeine:setIcon("caffeine-off.pdf")
+		caffeine:setIcon("~/.hammerspoon/caffeine-off.pdf")
 	end
 end
 
